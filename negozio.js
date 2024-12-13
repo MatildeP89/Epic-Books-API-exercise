95b37026035508095c3caf3163de3bfcb05d03b3
MatildeP89 <matildedlitam@gmail.com>
2024-12-13 01:03:42 +0100

    Add files via upload

---
+++ negozio.js
@@ -0,0 +1,7 @@
+let negozio = {
+    nomeNegozio: "Bookstore",
+    indirizzo: "Via Roma 123, 00100 Roma, Italia",
+    metodiPagamento: ["Carta di Credito", "PayPal", "Bonifico Bancario", "Contrassegno"],
+    speseSpedizione: 5.99, // in euro
+    sogliaSpedizioneGratuita: 50.00, // in euro
+    };     
